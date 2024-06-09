8b9fdb53b5e8edd33d1526933798f7fc3b5b3a9d
RohanKumarMainali <rohan.manali@aveshasystems.com>
2024-06-09 15:23:51 +0545

    feat(): simple grpc server with tls completed

---
+++ grpc-server/src/main/java/com/sadhan/grpcexample/service/Hello.java
@@ -0,0 +1,20 @@
+package com.sadhan.grpcexample.service;
+
+import com.example.service.grpc.HelloRequest;
+import com.example.service.grpc.HelloResponse;
+import com.example.service.grpc.HelloWorldServiceGrpc;
+
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+class GrettingService extends HelloWorldServiceGrpc.HelloWorldServiceImplBase {
+
+  @Override
+  public void sayHello(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
+    HelloResponse response = HelloResponse.newBuilder().setMessage("Hey " + request.getName()).build();
+    responseObserver.onNext(response);
+    responseObserver.onCompleted();
+  }
+
+}
---
+++ grpc-server/src/test/java/com/sadhan/grpcexample/GrpcExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sadhan.grpcexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
