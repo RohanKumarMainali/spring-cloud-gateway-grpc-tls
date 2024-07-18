59d4a179c55df45d2e165914fe02fc99e46e272d
RohanKumarMainali <rohanmainali39@gmail.com>
2024-07-18 20:05:30 +0545

    fix(): add grpc_client

---
+++ grpc_client/src/main/java/com/example/demo/grpc/GrpcClient.java
@@ -0,0 +1,11 @@
+package com.example.demo.grpc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import io.grpc.ManagedChannel;
+
+public class GrpcClient {
+
+  // @Autowired
+  // ManagedChannel managedChannel;
+}
---
+++ grpc_client/src/main/java/com/example/demo/grpc/GrpcController.java
@@ -0,0 +1,13 @@
+package com.example.demo.grpc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GrpcController {
+  // @Autowired
+  // GrpcClient grpcClient;
+
+  // private HelloWorldServiceBlockingStub bookServiceBlockingStub;
+
+}
