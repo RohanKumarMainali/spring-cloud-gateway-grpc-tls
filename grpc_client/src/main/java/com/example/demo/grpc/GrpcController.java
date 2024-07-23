ebb8e2e36473a178cf6bc9fc72e892093f86d785
RohanKumarMainali <rohanmainali39@gmail.com>
2024-07-23 21:11:47 +0545

    feat(): add client code

--- grpc_client/src/main/java/com/example/demo/grpc/GrpcController.java
+++ grpc_client/src/main/java/com/example/demo/grpc/GrpcController.java
@@ -1,13 +1,41 @@
 package com.example.demo.grpc;
 
+import com.example.service.grpc.HelloWorldServiceGrpc;
+import com.example.service.grpc.HelloWorldServiceGrpc.HelloWorldServiceBlockingStub;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.service.grpc.HelloRequest;
+import com.example.service.grpc.HelloResponse;
 
 @RestController
 public class GrpcController {
+
   // @Autowired
   // GrpcClient grpcClient;
+  // private HelloWorldServiceGrpc.HelloWorldServiceBlockingStub stub;
 
-  // private HelloWorldServiceBlockingStub bookServiceBlockingStub;
+  @GetMapping("/user")
+  public String user() {
+    // return "Hello User";
+
+    ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9091)
+        .usePlaintext()
+        .build();
+    //
+    final HelloWorldServiceBlockingStub stub = HelloWorldServiceGrpc.newBlockingStub(channel);
+    HelloResponse helloResponse = stub.sayHello(HelloRequest.newBuilder().setName("Rohan Kumar Mainali").build());
+    return helloResponse.getMessage();
+    //
+    // HelloResponse helloResponse =
+    // stub.sayHello(HelloRequest.newBuilder().setName("Rohan Kumar
+    // Mainali").build());
+    // System.out.println("Response " + helloResponse.getMessage());
+    // return helloResponse.getMessage();
+  }
 
 }
